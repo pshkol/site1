076617d8bdcf2650d197199435a43198fccf6855
Rasnor <pshkolnyy@outlook.com>
2017-01-03 21:32:45 -0300

    second commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,34 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
-var rename = require('gulp-rename')
+var rename = require('gulp-rename');
+var connect = require('gulp-connect');
+
+gulp.task('server', function() {
+    connect.server({
+        root: './',
+        livereload: true,
+        port: 80,
+    })
+})
+
+gulp.task('default',['server','css', 'css:watch', 'html:watch'])
 
 gulp.task('css', function() {
     return gulp.src('sass/main.sass').
             pipe(sass()).
             pipe(cleanCSS()).
             pipe(rename('main.min.css')).
-            pipe(gulp.dest('css'))
+            pipe(gulp.dest('css')). 
+            pipe(connect.reload())
+})
+
+gulp.task('reload', function() {
+    connect.reload();
+})
+
+gulp.task('html:watch', function() {
+    gulp.watch('index.html', ['reload'])
 })
 
 gulp.task('css:watch', function() {
